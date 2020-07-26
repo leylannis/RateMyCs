fc91c5d59fc937c0d0afadfbeff9e755b1a468cc
leylannis <loliface99@gmail.com>
2020-07-26 12:19:11 -0400

    adding of a review page exists - can add and clear next -submit button checks input is valid need to: - populate course code with appropriate code - enable adding to firebase via submit button

--- app/src/main/java/com/example/ratemycs/WriteReviewActivity.java
+++ app/src/main/java/com/example/ratemycs/WriteReviewActivity.java
@@ -7,18 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.graphics.Color.RED;
 
 public class WriteReviewActivity extends AppCompatActivity {
+    TextView courseCode;
+    EditText scoreView, professorView, descriptionView;
+    String code, score, professor, description;
+    Button submit, clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
-
-        TextView courseCode;
-        final EditText scoreView, professorView, descriptionView;
-        final String code, score, professor, description;
-        Button submit, clear;
 
         courseCode = findViewById(R.id.code_TextView);
         scoreView = findViewById(R.id.score_EditText);
@@ -35,5 +37,41 @@ public class WriteReviewActivity extends AppCompatActivity {
                 descriptionView.setText("");
             }
         });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // assign input values
+                code = courseCode.getText().toString();
+                score = scoreView.getText().toString();
+                professor = professorView.getText().toString();
+                description = descriptionView.getText().toString();
+                if(checkInput()){
+                    addReviewToDatabase();
+                }
+            }
+        });
+
+    }
+
+    private boolean checkInput(){
+        if (!score.equals("1") && !score.equals("2") && !score.equals("3") && !score.equals("4") && !score.equals("5")){
+            Toast.makeText(getApplicationContext(), "Please enter valid data for above fields\nRating must be 1-5", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(professor.equals("")){
+            Toast.makeText(getApplicationContext(), "Please enter valid data for above fields\nProfessor name is empty", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(description.equals("")){
+            Toast.makeText(getApplicationContext(), "Please enter valid data for above fields\nReview description is empty", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void addReviewToDatabase(){
+
+
     }
 }
