113f5ffe57f824e3ff98153c4991a4c984ed8f31
leylannis <loliface99@gmail.com>
2020-07-26 11:19:12 -0400

    adding of a review page exists - can add and clear next need to: - populate course code with appropriate code - add functionality for submit button

--- app/src/main/java/com/example/ratemycs/ReviewAdapter.java
+++ app/src/main/java/com/example/ratemycs/ReviewAdapter.java
@@ -1,7 +1,6 @@
 package com.example.ratemycs;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +8,6 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ReviewAdapter extends RecyclerView.Adapter<ReviewViewHolder>  {
--- app/src/main/java/com/example/ratemycs/WriteReviewActivity.java
+++ app/src/main/java/com/example/ratemycs/WriteReviewActivity.java
@@ -3,6 +3,10 @@ package com.example.ratemycs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WriteReviewActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class WriteReviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
+
+        TextView courseCode;
+        final EditText scoreView, professorView, descriptionView;
+        final String code, score, professor, description;
+        Button submit, clear;
+
+        courseCode = findViewById(R.id.code_TextView);
+        scoreView = findViewById(R.id.score_EditText);
+        professorView = findViewById(R.id.prof_EditText);
+        descriptionView = findViewById(R.id.desc_EditText);
+        submit = findViewById(R.id.submit_Button);
+        clear = findViewById(R.id.clear_Button);
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scoreView.setText("");
+                professorView.setText("");
+                descriptionView.setText("");
+            }
+        });
     }
 }
