efbfe3dab170ba541e4643dc35d1a4605f2b9fce
leylannis <loliface99@gmail.com>
2020-07-27 02:01:53 -0400

    Firebase User Authentication - login screen completed - successfully recognizes or adds user from/to database - drawer menu only displays login or logout, whichever is relevant to the state of a user being signed in

--- app/src/main/java/com/example/ratemycs/MainActivity.java
+++ app/src/main/java/com/example/ratemycs/MainActivity.java
@@ -16,7 +16,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     TextView textView;
     private DatabaseReference mDatabase;
     FirebaseDatabase db;
+    private FirebaseAuth firebaseAuth;
 
 
 
@@ -38,43 +43,57 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        navigationView=findViewById(R.id.nav_view);
-        toolbar=findViewById(R.id.toolbar);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        menu = navigationView.getMenu();
+
 
         setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.bringToFront();
 
         navigationView.setNavigationItemSelectedListener(this);
         loadFragment(new HomeFragment());
-        //navigationView.setCheckedItem(R.id.nav_home);
 
+        if (currentUser == null){
+            menu.findItem(R.id.nav_logout).setVisible(false);
+        }
+        else{
+            menu.findItem(R.id.nav_login).setVisible(false);
+        }
     }
 
     @Override
-    public void onBackPressed(){
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START);
-        }
-        else
+        } else
             super.onBackPressed();
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Fragment fragment;
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.nav_home:
                 fragment = new HomeFragment();
                 loadFragment(fragment);
                 break;
             case R.id.nav_login:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
                 break;
             case R.id.nav_logout:
+                firebaseAuth.signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
                 break;
             case R.id.nav_profile:
                 fragment = new ProfileFragment();
